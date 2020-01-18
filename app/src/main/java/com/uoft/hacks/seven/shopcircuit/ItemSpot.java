64c481e97bc1da0e1d05537f81a770f3415973c9
eric-li18 <eric.kh.li0821@gmail.com>
2020-01-18 15:23:20 -0500

    Eric Li: added itemspot class

---
+++ app/src/main/java/com/uoft/hacks/seven/shopcircuit/ItemSpot.java
@@ -0,0 +1,11 @@
+package com.uoft.hacks.seven.shopcircuit;
+
+public class ItemSpot {
+  private int x;
+  private int y;
+
+  public ItemSpot(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+}
